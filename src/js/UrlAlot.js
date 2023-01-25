54e716ace001bb1b0a8b6d59ef118862780e9cd5
Roberto Vargas <roberto.anemona@gmail.com>
2023-01-24 22:08:22 -0600

    cambios

--- src/js/UrlAlot.js
+++ src/js/UrlAlot.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Cookies from 'js-cookie';
 
-export var urlCookie = Cookies.get('url') == undefined ? "http://localhost/checker/" : Cookies.get('url');
+// export var urlCookie = Cookies.get('url') == undefined ? "http://localhost/checker/" : Cookies.get('url');
+export var urlCookie = "https://www.personalchecker.com/";
 // export var urlCookie = "https://www.personalchecker.com/";
-// export var urlCookie = "https://www.personalchecker.com/";
+1
